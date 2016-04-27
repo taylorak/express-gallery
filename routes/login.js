5f59966741cc6c987f67aa54e5aa2f7a69c91c29
Taylor Kennedy <taykenned@gmail.com>
2016-04-26 20:26:02 -1000

    adds bcrypt hashing to login routes

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-var passport = require('passport');
+const passport = require('passport');
 
 router.route('/')
   .get((req, res) => {
--- routes/register.js
+++ routes/register.js
@@ -1,10 +1,13 @@
 'use strict';
 
 const express = require('express');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
 const router = express.Router();
-var passport = require('passport');
 
 const User = require('../models').User;
+
+const saltRounds = 10;
 
 router.route('/')
   .get((req, res) => {
@@ -20,16 +23,18 @@ router.route('/')
         req.flash('error', 'user already exists');
         return res.redirect('/register');
       } else {
-        User.create({
-          username: username,
-          password: password
-        })
-        .then((user) => {
-          req.login(user, (err) => {
-            if(err) {
-              return next(err);
-            }
-            return res.redirect('/gallery');
+        bcrypt.hash(password, saltRounds, (err, hash) => {
+          User.create({
+            username: username,
+            password: hash
+          })
+          .then((user) => {
+            req.login(user, (err) => {
+              if(err) {
+                return next(err);
+              }
+              return res.redirect('/gallery');
+            });
           });
         });
       }
