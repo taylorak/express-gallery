fa9e05e21607691fb8273d8278f333413422bfbf
Taylor Kennedy <taykenned@gmail.com>
2016-04-26 14:33:33 -1000

    adds passport middleware

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
 const galleryRoute = require('./routes/gallery');
 const registerRoute = require('./routes/register');
 const loginRoute = require('./routes/login');
@@ -10,12 +17,17 @@ const loginRoute = require('./routes/login');
 const app = express();
 const db = require('./models');
 
-app.use(express.static('./public'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-
 app.set('view engine', 'jade');
 app.set('views', './views');
+
+app.use(cookieParser());
+app.use(flash());
+app.use(session({secret: 'catbutts', resave: false, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.use('/gallery', galleryRoute);
 app.use('/register', registerRoute);
@@ -30,5 +42,3 @@ db.sequelize.sync().then(() => {
     console.log('server running on port 3000');
   });
 });
-
-module.exports = app;
