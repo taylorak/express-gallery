5b5074c44d61c0f5ad23daad0bee810f6a3b8b1c
tpyoung <tylerparkeyoung@gmail.com>
2016-04-27 09:54:41 -1000

    Gallery page styled

--- seeders/20160425194432-photo_seed.js
+++ seeders/20160425194432-photo_seed.js
@@ -3,38 +3,45 @@
 module.exports = {
   up: function (queryInterface, Sequelize) {
       return queryInterface.bulkInsert('Photos', [{
-        link: 'https://farm2.staticflickr.com/1607/26043844443_ebe47640d0_o.jpg',
-        author: 'John Doe',
-        description: 'Lorem Ipsum',
+        link: 'http://payload.cargocollective.com/1/0/128/1388457/01-ottoman-architecture_905.jpg',
+        author: 'Ulya Vogt-Göknil',
+        description: 'From the 1966 book Living architecture: Ottoman',
         createdAt: new Date(),
         updatedAt: new Date()
-      }, {
-        link: 'http://i.imgur.com/1eBKy6i.jpg',
-        author: 'John Doe',
-        description: 'Lorem Ipsum',
-                createdAt: new Date(),
-        updatedAt: new Date()
-      }, {
-        link: 'http://i.imgur.com/AmWThvw.jpg',
-        author: 'John Doe',
-        description: 'Lorem Ipsum',
+      },  {
+        link: 'http://payload.cargocollective.com/1/0/128/1388457/07-ottoman-architecture_905.jpg',
+        author: 'Ulya Vogt-Göknil',
+        description: 'From the 1966 book Living architecture: Ottoman',
                 createdAt: new Date(),
         updatedAt: new Date()
 
       }, {
-        link: 'http://i.imgur.com/9nqsi0r.jpg',
-        author: 'John Doe',
-        description: 'Lorem Ipsum',
+        link: 'http://payload.cargocollective.com/1/0/128/1388457/10-ottoman-architecture_905.jpg',
+        author: 'Ulya Vogt-Göknil',
+        description: 'From the 1966 book Living architecture: Ottoman',
                 createdAt: new Date(),
         updatedAt: new Date()
 
       }, {
-        link: 'http://i.imgur.com/5CfVdO6.jpg',
-        author: 'John Doe',
-        description: 'Lorem Ipsum',
+        link: 'http://payload.cargocollective.com/1/0/128/1388457/04-ottoman-architecture_4_905.jpg',
+        author: 'Ulya Vogt-Göknil',
+        description: 'From the 1966 book Living architecture: Ottoman',
                 createdAt: new Date(),
         updatedAt: new Date()
 
+      },{
+        link: 'http://payload.cargocollective.com/1/0/128/1388457/08-ottoman-architecture_905.jpg',
+        author: 'Ulya Vogt-Göknil',
+        description: 'From the 1966 book Living architecture: Ottoman',
+                createdAt: new Date(),
+        updatedAt: new Date()
+
+      }, {
+        link: 'http://payload.cargocollective.com/1/0/128/1388457/03-ottoman-architecture_905.jpg',
+        author: 'Ulya Vogt-Göknil',
+        description: 'From the 1966 book Living architecture: Ottoman',
+                createdAt: new Date(),
+        updatedAt: new Date()
       } ], {});
   },
 
