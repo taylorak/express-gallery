d557d1e604b28b4cc72a9a79b17945b06dcb9627
tpyoung <tylerparkeyoung@gmail.com>
2016-04-29 11:21:39 -1000

    development

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -2,10 +2,8 @@ const Photo = require('../models').Photo;
 
 var authentication = {
   yourPhoto: (req, res, next) => {
-        // console.log('REQ USER ', req.user);
     Photo.findById(req.params.id)
       .then((photo)=> {
-        // console.log('PHOTO', photo);
         if (req.user.id === photo.user_id) {
           return next();
         }
