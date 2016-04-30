0cc299f3c20789eae7a6fef381e594422409673c
tpyoung <tylerparkeyoung@gmail.com>
2016-04-29 15:24:03 -1000

    delete and edit method finished

--- public/js/buttonEvents.js
+++ public/js/buttonEvents.js
@@ -1,46 +1,31 @@
-var deleteButton = document.querySelector('#deleteButton');
 var editButton = document.querySelector('#editButton');
+var editForm = document.querySelector('#blurDiv');
+var cancelButton = document.querySelector('#cancelButton');
 
-deleteButton.addEventListener('click', deleteButtonClick);
-editButton.addEventListener('click',editButtonClick);
-
-function deleteButtonClick(evt) {
-  evt.preventDefault();
-  // console.log('CLICK', this.href);
-  // var xhr = new XMLHttpRequest();
-  // xhr.addEventListener('load', deletePhoto);
-  // xhr.open('DELETE', this.href);
-  // xhr.send();
-}
+editButton.addEventListener('click', editButtonClick);
+cancelButton.addEventListener('click', cancelClick);
 
 function editButtonClick(evt) {
   evt.preventDefault();
-  var xhr = new XMLHttpRequest();
-  xhr.addEventListener('load', deletePhoto);
-  xhr.open('PUT', this.href);
-  xhr.send();
-}
-
-
-function deletePhoto(evt) {
-  console.log("delete Photo");
-  var response = JSON.parse(this.responseText);
-  if(response.success === true) {
-    window.location.href = response.redirect;
+  if (editForm.className === 'displayNone') {
+    editForm.className = 'blurCover';
+    window.location.href = editButton.href;
   }
 }
 
-function editPhoto(evt) {
-  var response = JSON.parse(this.responseText);
-  if(response.success === true) {
-    window.location.href = response.redirect;
+function cancelClick(evt) {
+  evt.preventDefault();
+  if (editForm.className === 'blurCover') {
+    editForm.className = 'displayNone';
   }
 }
 
-function editShow(){
-  document.getElementById("photoBlur").className = "editShow";
-}
 
-function editHide(){
-  document.getElementById("photoBlur").className = "editHide";
-}
+
+
+// function editPhoto(evt) {
+//   var response = JSON.parse(this.responseText);
+//   if(response.success === true) {
+//     window.location.href = response.redirect;
+//   }
+// }
