5f056f2805589b67dedc21f4c3840b443f3b60df
Taylor Kennedy <taykenned@gmail.com>
2016-05-29 17:06:59 -1000

    removes console log

--- models/index.js
+++ models/index.js
@@ -8,7 +8,6 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
-console.log(process.env);
 if(process.env.DATABASE_URL) {
   var sequelize = new Sequelize(process.env.DATABASE_URL);
 } else {
